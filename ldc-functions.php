bcfa290c4feaeef6fd833bdd2907059bc39d204d
ifwp <ifwp@vdsoe.com>
2021-01-25 18:18:43 -0600

    LDC

--- ldc-functions.php
+++ ldc-functions.php
@@ -10,7 +10,7 @@ Network:
 Plugin Name: LDC Functions
 Plugin URI: https://github.com/luisdelcid/ldc-functions
 Text Domain: ldc-functions
-Version: 0.1.25.2
+Version: 0.1.25.3
 */
 
 if(defined('ABSPATH')){
