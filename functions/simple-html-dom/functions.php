3e48dd7c7e275450368ac1464193ce236956d899
vidsoe <labs@vidsoe.com>
2021-02-20 12:51:54 -0600

    LDC

--- functions/simple-html-dom/functions.php
+++ functions/simple-html-dom/functions.php
@@ -2,29 +2,35 @@
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-/*if(!function_exists('ldc_require_simple_html_dom')){
-    function ldc_require_simple_html_dom(){
-        if(!class_exists('simple_html_dom')){
-            $dir = ldc_upload_basedir() . '/github/simplehtmldom/simplehtmldom-1.9.1';
-            if(is_dir($dir)){
-                require_once($dir . '/simple_html_dom.php');
-                return true;
-            } else {
-                $url = 'https://github.com/simplehtmldom/simplehtmldom/archive/1.9.1.zip';
-                return ldc_download_and_unzip($url, $dir);
-            }
-		}
-        return ldc_error();
+if(!function_exists('ldc_use_simple_html_dom')){
+    function ldc_use_simple_html_dom(){
+        if(class_exists('simple_html_dom')){
+            return true;
+        }
+        $dir = ldc_upload_basedir() . '/github/simple-html-dom/1.9.1';
+        $file = $dir . '/simplehtmldom-1.9.1/simple_html_dom.php';
+        if(file_exists($file)){
+            require_once($file);
+            return true;
+        }
+        $url = 'https://github.com/simplehtmldom/simplehtmldom/archive/1.9.1.zip';
+        $result = ldc_download_and_unzip($dir, $url);
+        if(is_wp_error($result)){
+            return $result;
+        }
+        require_once($file);
+        return true;
     }
-}*/
+}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 if(!function_exists('ldc_file_get_html')){
     function ldc_file_get_html(...$args){
-        if(!class_exists('simple_html_dom')){
+        /*if(!class_exists('simple_html_dom')){
             require_once(plugin_dir_path(__FILE__) . 'simple-html-dom-1.9.1/simple_html_dom.php');
-		}
+		}*/
+        ldc_use_simple_html_dom();
         return file_get_html(...$args);
     }
 }
@@ -33,9 +39,10 @@ if(!function_exists('ldc_file_get_html')){
 
 if(!function_exists('ldc_str_get_html')){
     function ldc_str_get_html(...$args){
-        if(!class_exists('simple_html_dom')){
+        /*if(!class_exists('simple_html_dom')){
             require_once(plugin_dir_path(__FILE__) . 'simple-html-dom-1.9.1/simple_html_dom.php');
-		}
+		}*/
+        ldc_use_simple_html_dom();
         return str_get_html(...$args);
     }
 }
