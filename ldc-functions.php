be9373dedb9d1e6445ac0cc6f9cce3383a180c5b
vidsoe <labs@vidsoe.com>
2021-02-20 22:13:09 -0600

    LDC

--- ldc-functions.php
+++ ldc-functions.php
@@ -10,7 +10,7 @@ Network: true
 Plugin Name: LDC Functions
 Plugin URI: https://github.com/luisdelcid/ldc-functions
 Text Domain: ldc-functions
-Version: 0.2.20.6
+Version: 0.2.20.7
 */
 
 if(defined('ABSPATH')){
@@ -24,27 +24,26 @@ if(defined('ABSPATH')){
         if(!ldc_is_doing_heartbeat()){
             $dir = ldc_upload_basedir();
             wp_mkdir_p($dir);
-            if(is_admin()){
-                ldc_build_update_checker('https://github.com/luisdelcid/ldc-functions', __FILE__, 'ldc-functions');
-                ldc_enqueue_functions('admin');
-                ldc_on('admin_notices', function() use($dir){
-                    if(!function_exists('get_filesystem_method')){
-                        require_once(ABSPATH . 'wp-admin/includes/file.php');
-                    }
-                    if(!wp_is_writable($dir) or get_filesystem_method() != 'direct'){
-                        ldc_add_admin_notice('LDC Functions could not access filesystem directly.');
-                    }
-        		});
-            } else {
-                ldc_enqueue_functions('front-end');
+            if(!function_exists('get_filesystem_method')){
+                require_once(ABSPATH . 'wp-admin/includes/file.php');
             }
-            ldc_on('after_setup_theme', function(){
-                $file = get_stylesheet_directory() . '/ldc-functions.php';
-                if(file_exists($file)){
-                    require_once($file);
+            if(wp_is_writable($dir) and get_filesystem_method() == 'direct'){
+                if(is_admin()){
+                    ldc_build_update_checker('https://github.com/luisdelcid/ldc-functions', __FILE__, 'ldc-functions');
+                    ldc_enqueue_functions('admin');
+                } else {
+                    ldc_enqueue_functions('front-end');
                 }
-            });
-            ldc_do('ldc_functions');
+                ldc_on('after_setup_theme', function(){
+                    $file = get_stylesheet_directory() . '/ldc-functions.php';
+                    if(file_exists($file)){
+                        require_once($file);
+                    }
+                });
+                ldc_do('ldc_functions');
+            } else {
+                ldc_add_admin_notice('LDC Functions could not access filesystem directly.');
+            }
         }
     });
 }
