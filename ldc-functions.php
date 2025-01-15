e64fc1aec0a627b194eaac4abec14059888039df
Luis del Cid <luisdelcid@gmail.com>
2025-01-15 20:57:36 +0000

    LDC

--- ldc-functions.php
+++ ldc-functions.php
@@ -3,7 +3,7 @@
  * Plugin Name: LDC Functions
  * Plugin URI: https://luisdelcid.com
  * Description: A personal collection of useful functions for WordPress plugins and themes.
- * Version: 0.1.14
+ * Version: 0.1.15
  * Requires at least: 5.6
  * Requires PHP: 5.6
  * Author: Luis del Cid
